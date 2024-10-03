4151a4073ce2c44cba3ad0e5733f8f1065ba5a81
malikbarbier7 <159799378+malikbarbier7@users.noreply.github.com>
2024-10-03 15:32:51 +0200

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { LandingPage } from '@/components/landing-page'
+
+export default function Home() {
+  return (
+    <div className="min-h-screen w-full">
+      <LandingPage />
+    </div>
+  )
+}
