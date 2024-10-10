01f40f7bbadda6616c02ea63ed58c8b43e55d23a
malikbarbier7 <159799378+malikbarbier7@users.noreply.github.com>
2024-10-10 11:44:49 +0200

    fix landing page main line

--- components/landing-page.tsx
+++ components/landing-page.tsx
@@ -39,7 +39,7 @@ export function LandingPage() {
             <div className="flex flex-col items-center space-y-4 text-center">
               <div className="space-y-2">
                 <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none">
-                Better Todo: Get More Done, Achieve Your Goals
+                Get More Done, Achieve Your Goals
                 </h1>
                 <p className="mx-auto max-w-[700px] text-gray-500 md:text-xl dark:text-gray-400">
                 Streamline your life with Better Todo by bringing together everyday tasks, personal priorities, work to-dos, and upcoming projects in one clear overview.
